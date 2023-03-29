5234cbab794087e3eef1f1885a26d4bba937f660
dei27 <deivicb93@gmail.com>
2023-03-29 16:18:06 -0600

    Proyecto Creado MVC

---
+++ RickMorty/src/main/java/com/rickmorty/core/RickMortyApplication.java
@@ -0,0 +1,13 @@
+package com.rickmorty.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickMortyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RickMortyApplication.class, args);
+	}
+
+}
---
+++ RickMorty/src/test/java/com/rickmorty/core/RickMortyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rickmorty.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickMortyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
